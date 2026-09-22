1efc355420448731128688b7ca8a8d4ae8783c5e
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:42:28 -0400

    chore: update librarian w/ better enum docs (#1017)
    In this version of librarian we improve the generated documentation for
    `enum` so the possible evolution and usage of the enums is more obvious.
    For example, we recommend using `@unknown default:` so they get warnings
    when new values appear in the enum.

--- Sources/GoogleCloudConfigDeliveryV1/DeletionPropagationPolicy.swift
+++ Sources/GoogleCloudConfigDeliveryV1/DeletionPropagationPolicy.swift
@@ -19,6 +19,12 @@ import Foundation
 /// Deletion Propagation Policy determines what happens to the underlying
 /// Kubernetes resources on a cluster when the `FleetPackage` managing
 /// those resources no longer targets the cluster or is deleted.
+///
+/// - Note: Adding cases to this enumeration is not considered a breaking change.
+///   Always include an `@unknown default:` case when switching over this type.
+///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+///   expecting specific values to remain unparsed; future releases may promote
+///   them to named cases.
 public enum DeletionPropagationPolicy: Codable, Equatable, Sendable {
   /// Unspecified deletion propagation policy. Defaults to FOREGROUND.
   case unspecified
@@ -30,15 +36,21 @@ public enum DeletionPropagationPolicy: Codable, Equatable, Sendable {
   case orphan
   /// Encodes an unknown integer value.
   ///
-  /// The most common cause for an unknown values is for the service to send
+  /// The most common cause for an unknown value is for the service to send
   /// a value unknown to the library. We recommend you update your library to
   /// the latest version.
+  ///
+  /// - Warning: Do not pattern-match specific integer values in this case;
+  ///   future releases may promote them to named enum cases.
   case unknownIntValue(Int)
   /// Encodes an unknown string value.
   ///
-  /// The most common cause for an unknown values is for the service to send
+  /// The most common cause for an unknown value is for the service to send
   /// a value unknown to the library. We recommend you update your library to
   /// the latest version.
+  ///
+  /// - Warning: Do not pattern-match specific string literals in this case;
+  ///   future releases may promote them to named enum cases.
   case unknownStringValue(String)
 
   public init() {
